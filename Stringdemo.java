96f74bf6197f0db07835fe210740274e87ea3fed
revanth1807 <151707482+revanth1807@users.noreply.github.com>
2024-10-26 11:50:01 +0530

    Stringdemo.java

---
+++ Stringdemo.java
@@ -0,0 +1,11 @@
+public class Stringdemo{
+    public static void main(String args[]){
+        String s1="Welcome";
+        char ch[]={'R','e','v','a','n','t','h'};
+        String s2=new String(ch);
+        String s3=new String("Examples");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
